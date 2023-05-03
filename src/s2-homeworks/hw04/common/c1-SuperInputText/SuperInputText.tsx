714caaee197c95dd155d640514a281cd40204fd5
Yaroslav Mescheryakov <migyaroslav@gmail.com>
2023-05-03 19:59:21 +0300

    refact: update HW5

--- src/s2-homeworks/hw04/common/c1-SuperInputText/SuperInputText.tsx
+++ src/s2-homeworks/hw04/common/c1-SuperInputText/SuperInputText.tsx
@@ -41,7 +41,7 @@ const SuperInputText: React.FC<SuperInputTextPropsType> = ({
   };
 
 	const finalSpanClassName = `${s.error} ${spanClassName ? spanClassName : ''}`;
-  const finalInputClassName = `${s.input} ${error ? s.errorInput : s.superInput} ${className ? s.className : ''}`;
+  const finalInputClassName = `${s.input} ${error ? s.errorInput : s.superInput} ${className ? className : ''}`;
 
   return (
     <div className={s.inputWrapper}>
